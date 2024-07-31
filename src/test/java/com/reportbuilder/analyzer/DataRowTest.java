de1b0da294a2037af0cdeafe9a6c9ac36264e887
omaneh <122321976+omaneh@users.noreply.github.com>
2024-07-31 17:11:39 -0400

    Added one test case

--- src/test/java/com/reportbuilder/analyzer/DataRowTest.java
+++ src/test/java/com/reportbuilder/analyzer/DataRowTest.java
@@ -1,4 +1,17 @@
 package com.reportbuilder.analyzer;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.Assert.*;
+
+import static org.junit.Assert.*;
+
 public class DataRowTest {
+
+    @org.junit.Test
+    public void column_returnsCorrectProperty() {
+        int amount = 5;
+        assertEquals(5, amount);
+    }
 }
